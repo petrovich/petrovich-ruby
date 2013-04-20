486fcf218094ac5cfb8f6ec1c7ddf281bcf514b9
tanraya <demerest@gmail.com>
2013-04-21 00:41:48 +0600

    Specs & surnames database by @ustalov

--- spec/petrovich/case_rules_spec.rb
+++ spec/petrovich/case_rules_spec.rb
@@ -17,6 +17,13 @@ describe "Склонение сложных русских" do
       it { subject.lastname('Дубинка', :dative).should == 'Дубинке' }
       it { subject.lastname('Сирота', :dative).should == 'Сироте' }
       it { subject.lastname('Воевода', :dative).should == 'Воеводе' }
+
+      it { subject.lastname('Волож', :dative).should == 'Воложу' }
+      it { subject.lastname('Кравец', :dative).should == 'Кравцу' }
+      it { subject.lastname('Самотечний', :dative).should == 'Самотечнему' }
+      it { subject.lastname('Цой', :dative).should == 'Цою' }
+      it { subject.lastname('Шопен', :dative).should == 'Шопену' }
+      it { subject.lastname('Сосковец', :dative).should == 'Сосковцу' }
     end
   end
 
@@ -35,6 +42,11 @@ describe "Склонение сложных русских" do
       it { subject.lastname('Дубинка', :dative).should == 'Дубинке' }
       it { subject.lastname('Сирота', :dative).should == 'Сироте' }
       it { subject.lastname('Воевода', :dative).should == 'Воеводе' }
+      it { subject.lastname('Гулыга', :dative).should == 'Гулыге' }
+      it { subject.lastname('Дейнека', :dative).should == 'Дейнеке' }
+      it { subject.lastname('Джанджагава', :dative).should == 'Джанджагава' }
+      it { subject.lastname('Забейворота', :dative).should == 'Забейворота' }
+      it { subject.lastname('Окуджава', :dative).should == 'Окуджаве' }
     end
   end
 end
