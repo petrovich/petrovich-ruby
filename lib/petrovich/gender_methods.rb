c40d272de4ecc61e73576235d6c1c709b68fcbee
tanraya <demerest@gmail.com>
2015-11-28 22:04:44 +0500

    Remove old files

--- lib/petrovich/gender_methods.rb
+++
@@ -1,9 +0,0 @@
-module Petrovich
-  # Методы определения пола по ФИО
-  module GenderMethods
-    def detect_gender(opts)
-      opts = { firstname: nil, middlename: nil, lastname: nil }
-
-    end
-  end
-end
