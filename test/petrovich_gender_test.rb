3f5dac58135f1e9c5661b6723634d9039e68c13a
tanraya <demerest@gmail.com>
2015-11-29 17:41:30 +0500

    Update tests

--- test/petrovich_gender_test.rb
+++ test/petrovich_gender_test.rb
@@ -3,66 +3,66 @@ require_relative 'test_helper'
 
 describe Petrovich::Gender do
   it 'detects male gender by firstname' do
-    assert_equal :male, Petrovich::Gender.detect(firstname: 'Александр')
+    assert_equal :male, Petrovich(firstname: 'Александр').gender
   end
 
   it 'detects male gender by lastname' do
-    assert_equal :male, Petrovich::Gender.detect(lastname: 'Склифасовский')
+    assert_equal :male, Petrovich(lastname: 'Склифасовский').gender
   end
 
   it 'detects female gender by firstname' do
-    assert_equal :female, Petrovich::Gender.detect(firstname: 'Александра')
+    assert_equal :female, Petrovich(firstname: 'Александра').gender
   end
 
   it 'detects female gender by lastname' do
-    assert_equal :female, Petrovich::Gender.detect(lastname: 'Склифасовская')
+    assert_equal :female, Petrovich(lastname: 'Склифасовская').gender
   end
 
   it 'detects female gender by firstname and lastname' do
-    assert_equal :female, Petrovich::Gender.detect(firstname: 'Александра', lastname: 'Склифасовская')
+    assert_equal :female, Petrovich(firstname: 'Александра', lastname: 'Склифасовская').gender
   end
 
   it 'detects androgynous gender by firstname' do
-    assert_equal :androgynous, Petrovich::Gender.detect(firstname: 'Саша')
+    assert_equal :androgynous, Petrovich(firstname: 'Саша').gender
   end
 
   it 'detects androgynous gender by firstname and lastname' do
-    assert_equal :androgynous, Petrovich::Gender.detect(firstname: 'Саша', lastname: 'Андрейчук')
+    assert_equal :androgynous, Petrovich(firstname: 'Саша', lastname: 'Андрейчук').gender
   end
 
   it 'detects male gender by firstname and lastname' do
-    assert_equal :male, Petrovich::Gender.detect(firstname: 'Саша', lastname: 'Иванов')
+    assert_equal :male, Petrovich(firstname: 'Саша', lastname: 'Иванов').gender
   end
 
   it 'detects male gender by firstname, lastname and middlename' do
-    assert_equal :male, Petrovich::Gender.detect(firstname: 'Саша', lastname: 'Андрейчук', middlename: 'Олегович')
+    assert_equal :male, Petrovich(firstname: 'Саша', lastname: 'Андрейчук', middlename: 'Олегович').gender
   end
 
   it 'detects male gender by firstname and middlename' do
-    assert_equal :male, Petrovich::Gender.detect(firstname: 'Саша', middlename: 'Олегович')
+    assert_equal :male, Petrovich(firstname: 'Саша', middlename: 'Олегович').gender
   end
 
   it 'detects androgynous gender by lastname' do
-    assert_equal :androgynous, Petrovich::Gender.detect(lastname: 'Осипчук')
+    assert_equal :androgynous, Petrovich(lastname: 'Осипчук').gender
   end
 
   it 'detects male gender by middlename' do
-    assert_equal :male, Petrovich::Gender.detect(middlename: 'Олегович')
+    assert_equal :male, Petrovich(middlename: 'Олегович').gender
   end
 
   it 'detects female gender by middlename' do
-    assert_equal :female, Petrovich::Gender.detect(middlename: 'Олеговна')
+    assert_equal :female, Petrovich(middlename: 'Олеговна').gender
   end
 
   it 'fails with argument error test 1' do
-    -> { Petrovich::Gender.detect(xxx: 'yyy') }.must_raise ArgumentError
+    proc { Petrovich::Gender.detect(xxx: 'yyy') }.must_raise ArgumentError
   end
 
   it 'fails with argument error test 2' do
-    -> { Petrovich::Gender.detect('wrong args') }.must_raise ArgumentError
+    proc { Petrovich::Gender.detect('wrong args') }.must_raise ArgumentError
   end
 
   it 'fails with argument error test 3' do
-    -> { Petrovich::Gender.detect(firstname: nil, lastname: nil) }.must_raise ArgumentError
+    proc { Petrovich::Gender.detect(firstname: nil, lastname: nil) }.must_raise ArgumentError
   end
 end
